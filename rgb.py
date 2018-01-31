5edc067d7cb13311548269a96a2632556136e5f4
Quinn Harrison <m182472@usna.edu>
2018-01-30 22:54:14 -0500

    updated python script, currently getting syntax error code for a for loop

--- rgb.py
+++ rgb.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python
 
+import serial
 import pygame, sys
 from pygame.locals import*
 
+
+s = serial.Serial("/dev/ttyACM0")	#serial port that we read from
 def main():   # starts main function, don't use naked code!
 
   pygame.init()
@@ -12,14 +15,20 @@ def main():   # starts main function, don't use naked code!
   #also possible to use pygame.Rect to create a rectangle
   while True:
     
-  #find way to import color readings into variables r_led, etc.
+
+    l = s.readline()			#l reads from arduino code imported from serial (s.)
+    x = l.rstrip().split(",")		#splits values separated by commas
+    try:				#tells loop to try condition
+      rgb = [int(val) for val in x]	#passes in rgb values 
+
+      #help received from 1/C Davidson
+      screen.fill(rgb)			#screen fill turns the screen a color rgb
+      #pygame.display.flip()		#updates to new screen	  
     
-    pygame.Color(r_led,g_led,b_led)	 #how colors are used in pygame
-    
-    
-    for event in pygame.event.get():
-      if event.type == QUIT:
+  #while True:    
+    for event in pygame.event.get():	#records "events" in pygame (keyboard, clicks, etc)
+      if event.type == QUIT:		#If event is close the window, quit the game	
         pygame.quit()
         sys.exit()
-    pygame.display.update()
+    pygame.display.update()		#update screen with new values
     fpsClock.tick(FPS)
