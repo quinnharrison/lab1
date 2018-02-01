5e1bcc38d7add55a418205f60e1a47a79b44d3f2
Quinn Harrison <m182472@usna.edu>
2018-01-31 19:15:38 -0500

    code should be good, but having issues with the serial connection. Program runs, but no screen is created

--- rgb.py
+++ rgb.py
@@ -6,29 +6,31 @@ from pygame.locals import*
 
 
 s = serial.Serial("/dev/ttyACM0")	#serial port that we read from
-def main():   # starts main function, don't use naked code!
-
+def main():   				#starts main function, don't use naked code!
+  print("int main")
   pygame.init()
-  FPS = 30  #set FPS to 30
-  fpsClock = pygame.time.Clock()
-  DISPLAYSURF = pygame.display.set_mode((400, 300))
+  #FPS = 30  #set FPS to 30
+  #fpsClock = pygame.time.Clock()
+  DISPLAYSURF = pygame.display.set_mode((400, 300))	#DISPLAYSURF is just a variable
   #also possible to use pygame.Rect to create a rectangle
   while True:
     
 
     l = s.readline()			#l reads from arduino code imported from serial (s.)
     x = l.rstrip().split(",")		#splits values separated by commas
+    
     try:				#tells loop to try condition
       rgb = [int(val) for val in x]	#passes in rgb values 
 
       #help received from 1/C Davidson
-      screen.fill(rgb)			#screen fill turns the screen a color rgb
+      DISPLAYSURF.fill(rgb)			#screen fill turns the screen a color rgb
       #pygame.display.flip()		#updates to new screen	  
-    
+    except ValueError:
+      pass
   #while True:    
     for event in pygame.event.get():	#records "events" in pygame (keyboard, clicks, etc)
       if event.type == QUIT:		#If event is close the window, quit the game	
         pygame.quit()
         sys.exit()
     pygame.display.update()		#update screen with new values
-    fpsClock.tick(FPS)
+    #fpsClock.tick(FPS)
